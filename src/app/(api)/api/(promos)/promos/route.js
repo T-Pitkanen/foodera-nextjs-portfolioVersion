e1f92a3c4e07610f815167bf4bfce0011953906e
TiiaP <123360783+TiiaPitkanen@users.noreply.github.com>
2024-04-15 09:04:35 +0200

    Update

--- src/app/(api)/api/(promos)/promos/route.js
+++
@@ -1,9 +0,0 @@
-// import { getPromos } from "@/lib/data.service";
-// import { NextResponse } from "next/server"
-
-// export async function GET() {
-
-//     let promos = await getPromos();
-//     return NextResponse.json(promos);
-
-// }
